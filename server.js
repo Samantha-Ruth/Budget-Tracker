8171ff952b034a0f728c9e9dd6e9a48634644f22
Samantha-Ruth <sam.haberman@gmail.com>
2022-08-21 23:56:31 -0600

    double checking

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
 
 app.use(express.json());
